0b0f566a784dccfec14fb809edea0af94902ca1e
z5206032 <z5206032@cse.unsw.edu.au>
2021-06-07 17:19:38 +1000

    Implement and test read_lexicon()

--- source/word_ladder.cpp
+++ source/word_ladder.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <fstream>
 #include <queue>
 #include <stdexcept>
 
@@ -8,10 +9,26 @@ namespace word_ladder {
 
 	struct Graph {
 		// std::vector < std::pair <
+
+		auto breadth_first_search() -> void {
+			std::deque<int> q; // we use a deque to get that sweet iterator
+
+			while (q.empty() == false) {
+				int v = q[0];
+				q.pop_front();
+
+				if (v == 0) {
+				}
+
+				for (const auto& n : q) {
+					q.push_back(n);
+				}
+			}
+		}
 	};
 
 	// Returns a vector of which characters are already matching between "from" and "to".
-	static auto compare_words(std::string const& from, std::string const& to) -> std::vector<bool> {
+	auto compare_words(std::string const& from, std::string const& to) -> std::vector<bool> {
 		if (from.length() != to.length()) {
 			throw std::runtime_error("Start and finishing words have different length.");
 		}
@@ -28,24 +45,15 @@ namespace word_ladder {
 		return vec;
 	}
 
-	static auto breadth_first_search() -> void {
-		std::queue<int> q;
-
-		while (q.empty() == false) {
-			int v = q[0];
-			q.pop();
-
-			if (9) {
-			}
-
-			for (auto& n : q) {
-				q.push(n);
-			}
-		}
-	}
-
 	// Get the lexicon as an unordered set of strings from the file specified by the path.
-	[[nodiscard]] auto read_lexicon(std::string const& path) -> std::unordered_set<std::string> {}
+	[[nodiscard]] auto read_lexicon(std::string const& path) -> std::unordered_set<std::string> {
+		auto file = std::ifstream(path);
+		auto lexicon = std::unordered_set<std::string>();
+		std::copy(std::istream_iterator<std::string>(file),
+		          std::istream_iterator<std::string>(),
+		          std::inserter(lexicon, lexicon.begin()));
+		return lexicon;
+	}
 
 	// Returns a vector of vectors of strings. Inner vector is a word ladder where each word in the
 	// ladder is a valid word from the lexicon. Outer vector contains other possible word ladders.
@@ -60,10 +68,12 @@ namespace word_ladder {
 	                            std::string const& to,
 	                            std::unordered_set<std::string> const& lexicon)
 	   -> std::vector<std::vector<std::string>> {
+		auto word_ladders = std::vector<std::vector<std::string>>();
 		// Which letters of the word are already the same.
 
 		// Filter lexicon for only words of the same length.
 
 		// BFS
+		return word_ladders;
 	}
 } // namespace word_ladder
