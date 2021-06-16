f07adcad1bc54be6390ecf3411e01c6682a0c4ed
z5206032 <z5206032@cse.unsw.edu.au>
2021-06-16 20:38:09 +1000

    Add test

--- test/word_ladder/test_word_ladder_multiple.cpp
+++ test/word_ladder/test_word_ladder_multiple.cpp
@@ -35,7 +35,8 @@ TEST_CASE("Words with a few word ladder solutions") {
 		auto const ladders = word_ladder::generate("sour", "vise", english_lexicon);
 		auto const answers = std::vector<std::vector<std::string>>{
 		   {"sour", "soup", "roup", "roue", "rose", "rise", "vise"},
-		   {"sour", "soup", "sols", "sole", "vole", "vile", "vise"}};
+		   {"sour", "soup", "sols", "sole", "vole", "vile", "vise"},
+		};
 
 		CHECK(ladders == answers);
 	}
@@ -44,7 +45,8 @@ TEST_CASE("Words with a few word ladder solutions") {
 		auto const ladders = word_ladder::generate("apple", "peach", english_lexicon);
 		auto const answers = std::vector<std::vector<std::string>>{
 		   {"awake", "aware", "sware", "share", "sharn", "shawn", "shewn", "sheen", "sheep", "sleep"},
-		   {"awake", "aware", "sware", "share", "shire", "shirr", "shier", "sheer", "sheep", "sleep"}};
+		   {"awake", "aware", "sware", "share", "shire", "shirr", "shier", "sheer", "sheep", "sleep"},
+		};
 
 		CHECK(ladders == answers);
 	}
@@ -52,6 +54,25 @@ TEST_CASE("Words with a few word ladder solutions") {
 
 TEST_CASE("Words with many word ladder solutions") {
 	auto const english_lexicon = word_ladder::read_lexicon("./english.txt");
+
+	SECTION("try -> rat") {
+		auto const ladders = word_ladder::generate("try", "rat", english_lexicon);
+		auto const answers = std::vector<std::vector<std::string>>{
+		   {"try", "cry", "cay", "cat", "rat"},
+		   {"try", "cry", "cay", "ray", "rat"},
+		   {"try", "dry", "day", "ray", "rat"},
+		   {"try", "fry", "fay", "fat", "rat"},
+		   {"try", "fry", "fay", "ray", "rat"},
+		   {"try", "pry", "pay", "pat", "rat"},
+		   {"try", "pry", "pay", "ray", "rat"},
+		   {"try", "toy", "tot", "rot", "rat"},
+		   {"try", "toy", "tot", "tat", "rat"},
+		   {"try", "wry", "way", "ray", "rat"},
+		   {"try", "wry", "wau", "wat", "rat"},
+		};
+
+		CHECK(ladders == answers);
+	}
 
 	SECTION("work -> play") {
 		auto const ladders = word_ladder::generate("apple", "peach", english_lexicon);
@@ -67,7 +88,8 @@ TEST_CASE("Words with many word ladder solutions") {
 		   {"work", "worn", "porn", "pirn", "pian", "plan", "play"},
 		   {"work", "wort", "bort", "boat", "blat", "plat", "play"},
 		   {"work", "wort", "port", "pert", "peat", "plat", "play"},
-		   {"work", "wort", "wert", "pert", "peat", "plat", "play"}};
+		   {"work", "wort", "wert", "pert", "peat", "plat", "play"},
+		};
 
 		CHECK(ladders == answers);
 	}
