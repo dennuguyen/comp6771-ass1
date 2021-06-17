97b2727d4a8571ca980f21a0e6d9ae659ead2aff
z5206032 <z5206032@cse.unsw.edu.au>
2021-06-17 22:12:15 +1000

    Verify testing method

--- test/word_ladder/test_word_ladder_duplicates.cpp
+++ test/word_ladder/test_word_ladder_duplicates.cpp
@@ -31,6 +31,41 @@
 
 #include "catch2/catch.hpp"
 
+// I write this test case because I am paranoid about the correctness of the testing method for this
+// test file. The below sections shows various ways we can use std::adjacent_find().
+//
+// std::adjacent_find() == it.end() means that the STL had duplicate elements.
+// std::adjacent_find() != it.end() means that the STL did not have duplicate elements.
+TEST_CASE("Test that testing method is correct") {
+	// Gives us the expected output.
+	SECTION("Two consecutive and identical words") {
+		auto lexicon = std::vector<std::string>{"word", "word", "hello"};
+		CHECK(std::adjacent_find(lexicon.begin(), lexicon.end()) != lexicon.end());
+	}
+
+	// Check that the output is unexpected.
+	// This test shows us that we MUST sort before calling std::adjacent_find()
+	SECTION("Two non-conseuctive and identical words without sort") {
+		auto lexicon = std::vector<std::string>{"word", "hello", "word"};
+		CHECK(std::adjacent_find(lexicon.begin(), lexicon.end()) == lexicon.end());
+	}
+
+	// Gives us the expected output.
+	// This test shows us that we MUST sort before calling std::adjacent_find()
+	SECTION("Two non-conseuctive and identical words with sort") {
+		auto lexicon = std::vector<std::string>{"word", "hello", "word"};
+		std::sort(lexicon.begin(), lexicon.end());
+		CHECK(std::adjacent_find(lexicon.begin(), lexicon.end()) != lexicon.end());
+	}
+
+	// Gives us the expected output.
+	SECTION("Three non-identical words") {
+		auto lexicon = std::vector<std::string>{"word", "hello", "main"};
+		std::sort(lexicon.begin(), lexicon.end());
+		CHECK(std::adjacent_find(lexicon.begin(), lexicon.end()) == lexicon.end());
+	}
+}
+
 TEST_CASE("A word ladder should not contain duplicate words") {
 	auto const english_lexicon = word_ladder::read_lexicon("./english.txt");
 
@@ -46,7 +81,7 @@ TEST_CASE("A word ladder should not contain duplicate words") {
 	}
 
 	SECTION("trout -> mound") {
-		auto const ladders = word_ladder::generate("too", "two", english_lexicon);
+		auto ladders = word_ladder::generate("too", "two", english_lexicon);
 
 		REQUIRE(ladders.size() == 1);
 		auto ladder = ladders.front();
@@ -57,7 +92,7 @@ TEST_CASE("A word ladder should not contain duplicate words") {
 	}
 
 	SECTION("when -> what") {
-		auto const ladders = word_ladder::generate("when", "what", english_lexicon);
+		auto ladders = word_ladder::generate("when", "what", english_lexicon);
 
 		REQUIRE(ladders.size() == 1);
 		auto ladder = ladders.front();
@@ -77,7 +112,7 @@ TEST_CASE("All returned word ladders should not contain duplicate words") {
 		REQUIRE(ladders.size() > 1);
 		std::sort(ladders.begin(), ladders.end()); // must sort() before adjacent_find()
 
-		for (auto const& ladder : ladders) {
+		for (auto& ladder : ladders) {
 			CHECK(std::adjacent_find(ladder.begin(), ladder.end()) == ladder.end());
 		}
 	}
@@ -92,7 +127,7 @@ TEST_CASE("Word ladders should not contain duplicate word ladders") {
 		REQUIRE(ladders.size() > 1);
 		std::sort(ladders.begin(), ladders.end()); // must sort() before adjacent_find()
 
-		for (auto const& ladder : ladders) {
+		for (auto& ladder : ladders) {
 			CHECK(std::adjacent_find(ladder.begin(), ladder.end()) == ladder.end());
 		}
 	}
